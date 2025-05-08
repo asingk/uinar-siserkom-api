3f6e642ec46a9c90fceba31f474bf248c4ff43c1
asingk <asingk@proton.me>
2025-05-08 08:37:19 +0700

    fix #10

--- src/main/java/id/ac/arraniry/siserkomapi/util/GlobalConstants.java
+++ src/main/java/id/ac/arraniry/siserkomapi/util/GlobalConstants.java
@@ -4,7 +4,7 @@ public class GlobalConstants {
     public static final Integer JENIS_INVOICE_PELATIHAN = 1;
     public static final Integer JENIS_INVOICE_UJIAN = 2;
     public static final Integer JENIS_INVOICE_BUAT_SERTIFIKAT = 3;
-    public static final String[] NILAI_MK_LULUS = {"A+", "A", "A-", "B+", "B", "B-"};
+    public static final String[] NILAI_MK_LULUS = {"A", "A-", "B+", "B", "B-", "C+", "C"};
     public static final String SIAKAD_API_URL = "https://api.sevimaplatform.com/siakadcloud/v1";
     public static final String SEVIMA_JENIS_INVOICE_PELATIHAN = "018";
     public static final String SEVIMA_JENIS_INVOICE_UJIAN = "013";
@@ -12,7 +12,9 @@ public class GlobalConstants {
 
     // env staging
 //	public static final String SERTIFIKAT_URL = "http://192.168.176.233:3200/siserkom";
+//    public static final String CDN_SISERKOM_SERTIFIKAT_FOLDER = "/siserkom/staging_sertifikat";
 
     // env prod
     public static final String SERTIFIKAT_URL = "https://serdig.ar-raniry.ac.id/siserkom";
+    public static final String CDN_SISERKOM_SERTIFIKAT_FOLDER = "/siserkom/sertifikat";
 }
